716a295fb25cd5d8d107ab4630b56a976c6ed7f6
unknown <liliano.balde@htmail.com>
2023-11-20 11:58:35 +0000

    Committing index js and html

---
+++ index.js
@@ -0,0 +1,26 @@
+let age = 35
+console.log(age)
+
+let myName = "Leo"
+console.log(myName)
+
+let Portuguese = true
+console.log(Portuguese)
+
+let Location;
+console.log (Location)
+
+let BankAccount = null
+console.log (BankAccount)
+
+let Gender = "Female"
+console.log(Gender)
+
+Gender = "Male"
+console.log(Gender)
+
+const CurrentAccount = 0
+console.log(CurrentAccount);
+
+CurrentAccount = 10000000
+console.log(CurrentAccount);
